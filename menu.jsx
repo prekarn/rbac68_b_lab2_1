6e89d8d705b4a2ff8126d510224da6386d1ea5ed
prekarn <prekarn@mut.ac.th>
2025-11-07 10:22:00 +0700

    yai commit1

--- menu.jsx
+++ menu.jsx
@@ -1,3 +1,11 @@
 console.log("HOME");
 console.log("PRODUCT");
 console.log("ABOUT US");
+
+console.log("HOME");
+console.log("PRODUCT");
+console.log("ABOUT US");
+
+console.log("HOME");
+console.log("PRODUCT");
+console.log("ABOUT US");
