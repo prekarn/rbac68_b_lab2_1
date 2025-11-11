4d1f25550542e56651fc0a525676c59781249fed
prekarn <prekarn@mut.ac.th>
2025-11-11 10:19:12 +0700

    commit5

---
+++ payment.jsx
@@ -0,0 +1,3 @@
+console.log("QR code");
+console.log("True money");
+console.log("Credit card");
---
+++ product.jsx
@@ -0,0 +1,4 @@
+console.log("ดินสอ");
+console.log("ปากกา");
+console.log("สมุด");
+console.log("ยางลบ");
